8424d22aefe10dbd145fb0d6f0c2d40642ebaa07
Hotaro <asthetic4t5@gmail.com.com>
2026-02-26 14:16:59 +0530

    first commit

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -8,7 +8,7 @@ export async function GET(context: APIContext) {
   );
 
   return rss({
-    title: "UINUX Blog",
+    title: "HOTARO'S Blog",
     description: "Writing about systems, design, and building with restraint.",
     site: context.site!,
     items: posts.map((post) => ({
